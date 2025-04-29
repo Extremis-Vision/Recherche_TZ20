a9ed7c2458265ac0892f20357ce13457f2cfd0ec
Extremis-Vision <leonard.goetz@utbm.fr>
2025-04-29 14:29:17 +0200

    V1.05

--- 3_analyse_result/pandas_analyse.py
+++ 3_analyse_result/pandas_analyse.py
@@ -1,19 +1,27 @@
 import pandas as pd
-import ast
+import json
+from ast import literal_eval
 
-# Chargement des données
-df = pd.read_csv('/home/ghost/Documents/function_calling/3_analyse_result/benchmark_output_parser.csv')
+# Chargement du JSON
+with open('/home/ghost/Documents/function_calling/3_analyse_result/becnhmark_structured_output.json') as f:
+    data = json.load(f)
 
-print(df.head())
+# Conversion en DataFrame
+df = pd.json_normalize(data['results'])
 
+# Nettoyage des colonnes
+df['result'] = df['result'].apply(literal_eval)
+df['score'] = df['score'].apply(lambda x: sum(x)/len(x))  # Moyenne des scores
+df['time_metrics'] = df['time'].apply(lambda x: {k:v for d in x for k,v in d.items()})
 
-# Conversion des colonnes JSON
-test = df['time'].apply(ast.literal_eval)
-print(test)
+print(df.columns)
 
-#df['time'] = df['time'].apply(ast.literal_eval)
-#df['result'] = df['result'].apply(ast.literal_eval)
+# Extraction des métriques temporelles
+time_df = pd.json_normalize(df['time_metrics'])
+df = pd.concat([df, time_df], axis=1)
 
-# Feature engineering
-#df['temps_reponse_moyen'] = df['time'].apply(lambda x: (x['end_time'] - x['chargement_model']) / len(x) / 10)
- 
+# Calcul du débit (tokens/seconde)
+df['throughput'] = df['max_tokens'] / df['avg_reponse_time']
+
+# Découpage des résultats
+df = df.explode('result').reset_index(drop=True)
