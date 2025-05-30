f08876754534449dd1df5dd4884bca3068e6c465
Extremis-Vision <leonard.goetz@utbm.fr>
2025-05-30 16:47:05 +0200

    Division de Recherche correctement en fichier par classe

--- VersionFinale/BackEnd/Recherche/Crawler.py
+++ VersionFinale/BackEnd/Recherche/Crawler.py
@@ -1,31 +1,9 @@
 import requests
 import json
 import asyncio
-from typing import List, Optional, Dict, Any
-from dotenv import load_dotenv
-import os
-from langchain_community.utilities import SearxSearchWrapper
+from typing import List, Dict
 from crawl4ai import AsyncWebCrawler, CrawlerRunConfig, CacheMode
-
-load_dotenv()
-SEARCHURL = os.getenv("SEARCHURL")
-
-class RechercheBasique:
-    def __init__(self,engines : List[str] = ['wikipedia', 'bing', 'yahoo', 'google', 'duckduckgo']):
-        self.search = SearxSearchWrapper(searx_host=SEARCHURL)
-        self.engines = engines
-
-    def search_results(self, keyword: str, num_results: int = 5, engines: List[str] = None) -> List[Dict]:
-        engines = engines if engines is not None else self.engines
-        return self.search.results(keyword, engines=engines, num_results=num_results)
-
-    def multiplesearch(self, keywords: List[str], num_results: int = 5, engines: List[str] = None ) -> List[Dict]:
-        engines = engines if engines is not None else self.engines
-        results = []
-        for keyword in keywords:
-            results.extends(self.search_results(keyword,num_results,engines))
-        return results
-
+from .RechercheBasique import RechercheBasique
 
 class RechercheCrawling(RechercheBasique):        
     async def crawl_urls(self, url: str) -> List[Dict]:
@@ -49,20 +27,11 @@ class RechercheCrawling(RechercheBasique):
             print(f"Erreur inattendue lors du crawl de {url}: {e}")
             return []
 
-
-
-
     def get_websites_contend(self, urls: List[str], limit: int = 3) -> List[Dict]:
         data = []
         for url in urls[:limit]:
             data += asyncio.run(self.crawl_urls(url))
         return data
-
-        
-
-# Exemple utilisation de RechercheBasique
-#resultat_recherche = RechercheBasique()
-#print(resultat_recherche.search_results("Transformer",10))
 
 #Exemple utilisation de RechercheCrawling
 #urls = ["https://sbert.net/"]
---
+++ VersionFinale/BackEnd/Recherche/RechercheBasique.py
@@ -0,0 +1,27 @@
+from typing import List, Dict
+from dotenv import load_dotenv
+import os
+from langchain_community.utilities import SearxSearchWrapper
+
+load_dotenv()
+SEARCHURL = os.getenv("SEARCHURL")
+
+class RechercheBasique:
+    def __init__(self,engines : List[str] = ['wikipedia', 'bing', 'yahoo', 'google', 'duckduckgo']):
+        self.search = SearxSearchWrapper(searx_host=SEARCHURL)
+        self.engines = engines
+
+    def search_results(self, keyword: str, num_results: int = 5, engines: List[str] = None) -> List[Dict]:
+        engines = engines if engines is not None else self.engines
+        return self.search.results(keyword, engines=engines, num_results=num_results)
+
+    def multiplesearch(self, keywords: List[str], num_results: int = 5, engines: List[str] = None ) -> List[Dict]:
+        engines = engines if engines is not None else self.engines
+        results = []
+        for keyword in keywords:
+            results.extends(self.search_results(keyword,num_results,engines))
+        return results
+
+# Exemple utilisation de RechercheBasique
+#resultat_recherche = RechercheBasique()
+#print(resultat_recherche.search_results("Transformer",10))
