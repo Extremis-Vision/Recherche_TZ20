1f81bbd9df8fb862caf55f10577e16d94a9bfe2b
Extremis-Vision <leonard.goetz@utbm.fr>
2025-05-30 16:41:33 +0200

    Ajout exemple BDD fonctionnement/test

--- VersionFinale/BackEnd/exemple/test_BDD.py
+++ VersionFinale/BackEnd/exemple/test_BDD.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from BDD.BDD import Bdd
 from BDD.MotCle import MotCle
 
@@ -8,4 +13,3 @@ listes_mots = bdd.get_MotCle()
 for mot in listes_mots:
     print(mot)
     print(mot.supprimer(bdd))
-
